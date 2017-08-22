d1a3c47a52010d2503618b06100256744a426ada
S-Oleg-M <xwx18@mail.ru>
2017-08-22 21:43:26 +0300

    task1-full

--- js/pictures.js
+++ js/pictures.js
@@ -1,25 +1,62 @@
 'use strict';
 
-var myKekstagram = [];
 var commentsList = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+var template = document.querySelector('#picture-template').content.querySelector('.picture');
 
 // Находим рандомное число в диапазоне
-function getRandomInt(min, max) {
+var getRandomInt = function (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
-}
+};
 
 // Генерируем массив
-function generateArray() {
+var generateArray = function () {
+  var result = [];
   for (var i = 0; i < 25; i++) {
     var myKekstagramItem = {
       url: 'photos/' + (i + 1) + '.jpg',
       likes: getRandomInt(15, 201),
       comments: commentsList[getRandomInt(0, 6)]
     };
-    myKekstagram[i] = myKekstagramItem;
+    result[i] = myKekstagramItem;
   }
-}
+  return result;
+};
 
-generateArray();
+var myKekstagram = generateArray();
 
-console.log(myKekstagram)
+// Наполняем фрагмент елементами
+var getFragment = function () {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < 25; i++) {
+    var element = template.cloneNode(true);
+    element.children[0].setAttribute('src', myKekstagram[i].url);
+    element.querySelector('.picture-likes').textContent = myKekstagram[i].likes;
+    element.querySelector('.picture-comments').textContent = myKekstagram[i].comments;
+    fragment.appendChild(element);
+  }
+  return fragment;
+};
+
+var fragment = getFragment();
+
+// Вставляем фрагмент
+var insertFragment = function () {
+  var pictures = document.querySelector('.pictures');
+  pictures.appendChild(fragment);
+};
+
+insertFragment();
+
+var hideForm = document.querySelector('.upload-overlay');
+hideForm.classList.add('hidden');
+
+var showGallery = document.querySelector('.gallery-overlay');
+showGallery.classList.remove('hidden');
+
+var showGalleryContent = function () {
+  showGallery.querySelector('.gallery-overlay-image').setAttribute('src', myKekstagram[0].url);
+  showGallery.querySelector('.likes-count').textContent = myKekstagram[0].likes;
+  showGallery.querySelector('.comments-count').textContent = myKekstagram[0].comments;
+};
+
+showGalleryContent();
