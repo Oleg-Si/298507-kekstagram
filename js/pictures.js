5bae159e6684a221d1281054dd1d60f19fc15683
S-Oleg-M <xwx18@mail.ru>
2017-08-24 12:24:02 +0300

    help bug fix

--- js/pictures.js
+++ js/pictures.js
@@ -1,11 +1,27 @@
 'use strict';
 
-var COMMENTS_LIST = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+var COMMENTS_LIST = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
 var template = document.querySelector('#picture-template').content.querySelector('.picture');
 
 // Находим рандомное число в диапазоне
 var getRandomInt = function (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
+};
+
+var getCommentsNumbers = function () {
+  var commentsNumbers = [];
+  var commentsCount = getRandomInt(1, 3);
+  for (var i = 1; i <= commentsCount; i++) {
+    commentsNumbers.appendChild(COMMENTS_LIST[getRandomInt(0, 6)]);
+  }
+  return commentsNumbers;
 };
 
 // Генерируем массив
@@ -15,7 +31,7 @@ var generateArray = function () {
     var myKekstagramItem = {
       url: 'photos/' + (i + 1) + '.jpg',
       likes: getRandomInt(15, 201),
-      comments: COMMENTS_LIST[getRandomInt(0, 6)]
+      comments: getCommentsNumbers().length
     };
     result[i] = myKekstagramItem;
   }
@@ -52,6 +68,7 @@ hideForm.classList.add('hidden');
 
 var showGallery = document.querySelector('.gallery-overlay');
 showGallery.classList.remove('hidden');
+
 
 var showGalleryContent = function () {
   showGallery.querySelector('.gallery-overlay-image').setAttribute('src', myKekstagram[0].url);
