63b02dec534155a40b7380a9052069d45eff3c8c
S-Oleg-M <xwx18@mail.ru>
2017-08-29 11:23:57 +0300

    module4-task2 fin

--- js/pictures.js
+++ js/pictures.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var RESIZE_MAX_VALUE = 100;
+var RESIZE_MIN_VALUE = 25;
+var RISIZE_VALUE_STEP = 25;
 var ESC_KEYCODE = 27;
 var ENTER_KEYCODE = 13;
 var COMMENTS_LIST = [
@@ -81,7 +84,7 @@ var onGalleryEscPress = function (evt) {
 };
 
 // Показываем галерею
-var showGallery = function (i) {
+var showGallery = function () {
   galleryOverlay.classList.remove('hidden');
   document.addEventListener('keydown', onGalleryEscPress);
 };
@@ -128,13 +131,136 @@ for (var i = 0; i < 25; i++) {
 }
 
 // Добавляем обработчик клика мыши
-pictureClosed.addEventListener('click', function (evt) {
-  hideGallery();
-});
+pictureClosed.addEventListener('click', hideGallery);
 
 // Добавляем обработчик клика кнопки
 pictureClosed.addEventListener('keydown', function (evt) {
   if (evt.keyCode === ENTER_KEYCODE) {
     hideGallery();
   }
+});
+
+var uploadForm = document.querySelector('#upload-select-image');
+var uploadFile = uploadForm.querySelector('#upload-file');
+var uploadOverlay = uploadForm.querySelector('.upload-overlay');
+var uploadImage = uploadForm.querySelector('.upload-image');
+var uploadFin = uploadForm.querySelector('.upload-form-cancel');
+var uploadFormDescr = uploadForm.querySelector('.upload-form-description');
+uploadFormDescr.setAttribute('tabindex', 0);
+
+// Обработчик нажатия кнопки Esc на окне загрузки
+var onUploadEscPress = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    if (document.activeElement !== uploadFormDescr) {
+      onClickUploadFin();
+    }
+  }
+};
+
+var onChangeUploadForm = function () {
+  uploadOverlay.classList.remove('hidden');
+  uploadImage.classList.add('hidden');
+  document.addEventListener('keydown', onUploadEscPress);
+};
+
+var onClickUploadFin = function () {
+  uploadOverlay.classList.add('hidden');
+  uploadImage.classList.remove('hidden');
+  document.removeEventListener('keydown', onUploadEscPress);
+};
+
+uploadFile.addEventListener('change', onChangeUploadForm);
+
+uploadFin.addEventListener('click', onClickUploadFin);
+uploadFin.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    onClickUploadFin();
+  }
+});
+
+var resizeControlsLabel = uploadForm.querySelector('.upload-resize-controls-value');
+var buttonResizeInc = uploadForm.querySelector('.upload-resize-controls-button-inc');
+var buttonResizeDec = uploadForm.querySelector('.upload-resize-controls-button-dec');
+var uploadImageScale = uploadForm.querySelector('.effect-image-preview');
+var uploadEffectControls = uploadForm.querySelector('.upload-effect-controls');
+var uploadFormHashtags = uploadForm.querySelector('.upload-form-hashtags');
+
+// Изменяем размер фото
+var changeResizeValue = function (direction) {
+  var resizeControlsValue = parseInt(resizeControlsLabel.getAttribute('value'), 10);
+  var newResizeValue = resizeControlsValue + RISIZE_VALUE_STEP * direction;
+  if (newResizeValue >= RESIZE_MIN_VALUE && newResizeValue <= RESIZE_MAX_VALUE) {
+    resizeControlsLabel.setAttribute('value', newResizeValue + '%');
+    uploadImageScale.style.transform = 'scale(' + newResizeValue / 100 + ')';
+  }
+};
+
+buttonResizeInc.addEventListener('click', function () {
+  changeResizeValue(1);
+});
+buttonResizeDec.addEventListener('click', function () {
+  changeResizeValue(-1);
+});
+
+var currentEffect;
+var onClickImageEffect = function (target) {
+  var effectName = target.value;
+  uploadImageScale.classList.remove(currentEffect);
+  currentEffect = 'effect-' + effectName;
+  uploadImageScale.classList.add(currentEffect);
+};
+
+uploadEffectControls.addEventListener('click', function (evt) {
+  var target = evt.target;
+  if (target.tagName.toLowerCase() === 'input') {
+    onClickImageEffect(target);
+  }
+});
+
+// Проверяем повторяющиеся теги
+var onCheckForTheSameWord = function (listTags, index) {
+  var lengthListTags = listTags.length;
+  for (var j = 1; j < lengthListTags; j++) {
+    if (listTags[j] === listTags[index] && j !== index) {
+      uploadFormHashtags.setCustomValidity('Теги не должны повторяться');
+      break;
+    }
+  }
+};
+
+// Проверяем поле хеш-тегов
+var onCheckHashTags = function () {
+  var maxHashTags = 5;
+  var maxLengthTag = 21;
+  var tagsFieldValue = uploadFormHashtags.value;
+  var listHashTag = tagsFieldValue.match(/\#[a-zA-Zа-яА-Я0-9\-]+/g);
+
+  uploadFormHashtags.setCustomValidity('');
+
+  if (tagsFieldValue.length === 0) {
+    return;
+  }
+
+  if (listHashTag === null) {
+    uploadFormHashtags.setCustomValidity('Первый символ должен быть решеткой');
+  } else {
+    var lengthListHashTags = listHashTag.length;
+    if (lengthListHashTags > maxHashTags) {
+      uploadFormHashtags.setCustomValidity('Нелья добавить более 5 хеш-тегов');
+    }
+
+    for (var l = 0; l < lengthListHashTags; l++) {
+      if (listHashTag[l].length > maxLengthTag) {
+        uploadFormHashtags.setCustomValidity('Длина тега не должна превышать 20 символов');
+        break;
+      }
+      if (lengthListHashTags > 1) {
+        onCheckForTheSameWord(listHashTag, l);
+      }
+    }
+  }
+};
+
+uploadFormHashtags.addEventListener('input', function () {
+  onCheckHashTags();
 });
