916b7b85a28a4604e4c1607667ea83337275b60a
S-Oleg-M <xwx18@mail.ru>
2017-08-31 00:18:38 +0300

    help

--- js/pictures.js
+++ js/pictures.js
@@ -5,53 +5,60 @@ var RESIZE_MIN_VALUE = 25;
 var RISIZE_VALUE_STEP = 25;
 var ESC_KEYCODE = 27;
 var ENTER_KEYCODE = 13;
-var COMMENTS_LIST = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-];
-var template = document.querySelector('#picture-template').content.querySelector('.picture');
 
 // Находим рандомное число в диапазоне
 var getRandomInt = function (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 };
 
-// Генерируем массив комментариев
-var getCommentsNumbers = function () {
-  var commentsNumbers = [];
-  var commentsCount = getRandomInt(1, 3);
-  for (var i = 1; i <= commentsCount; i++) {
-    commentsNumbers[i - 1] = COMMENTS_LIST[getRandomInt(0, 6)];
-  }
-  return commentsNumbers;
-};
+/* -----------------------Модуль data.js-------------------------------- */
+(function () {
+  var COMMENTS_LIST = [
+    'Всё отлично!',
+    'В целом всё неплохо. Но не всё.',
+    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+  ];
 
-// Генерируем массив
-var generateArray = function () {
-  var result = [];
-  for (var i = 0; i < 25; i++) {
-    var myKekstagramItem = {
-      url: 'photos/' + (i + 1) + '.jpg',
-      likes: getRandomInt(15, 201),
-      comments: getCommentsNumbers().length
-    };
-    result.push(myKekstagramItem);
-  }
-  return result;
-};
+  // Генерируем массив комментариев
+  var getCommentsNumbers = function () {
+    var commentsNumbers = [];
+    var commentsCount = getRandomInt(1, 3);
+    for (var i = 1; i <= commentsCount; i++) {
+      commentsNumbers[i - 1] = COMMENTS_LIST[getRandomInt(0, 6)];
+    }
+    return commentsNumbers;
+  };
 
-var myKekstagram = generateArray();
+  window.data = {
+    // Генерируем массив
+    myKekstagram: function () {
+      var result = [];
+      for (var i = 0; i < 25; i++) {
+        var myKekstagramItem = {
+          url: 'photos/' + (i + 1) + '.jpg',
+          likes: getRandomInt(15, 201),
+          comments: getCommentsNumbers().length
+        };
+        result.push(myKekstagramItem);
+      }
+      return result;
+    }
+  };
+
+})();
+/* -------------------------Модуль data.js конец------------------------------ */
+
+var template = document.querySelector('#picture-template').content.querySelector('.picture');
 
 // Наполняем фрагмент елементами
 var getFragment = function () {
   var fragment = document.createDocumentFragment();
   for (var i = 0; i < 25; i++) {
     template.setAttribute('tabindex', 0);
-    template.children[0].setAttribute('src', myKekstagram[i].url);
+    template.children[0].setAttribute('src', data.myKekstagram.url);
     template.querySelector('.picture-likes').textContent = myKekstagram[i].likes;
     template.querySelector('.picture-comments').textContent = myKekstagram[i].comments;
     var element = template.cloneNode(true);
@@ -62,6 +69,7 @@ var getFragment = function () {
 
 var fragment = getFragment();
 
+
 // Вставляем фрагмент
 var insertFragment = function () {
   var pictures = document.querySelector('.pictures');
@@ -69,6 +77,8 @@ var insertFragment = function () {
 };
 
 insertFragment();
+
+
 
 var galleryOverlay = document.querySelector('.gallery-overlay');
 
